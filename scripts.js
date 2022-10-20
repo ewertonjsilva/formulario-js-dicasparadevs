338b5b98e96f2782eea2d515eb9e00fac4ee660f
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2022-10-20 09:21:31 -0300

    Comentario RegExp

--- scripts.js
+++ scripts.js
@@ -76,6 +76,7 @@ function setSuccessFor(input) {
 }
 
 function checkEmail(email) {
+  //  RegExp (Regular expressions): https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Guide/Regular_Expressions
   return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
     email
   );
